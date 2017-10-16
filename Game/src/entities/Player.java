3fd5e91e8f17705a6ba814fa4ed78dafff23331e
David <David@LAPTOP-QV3C70DQ>
2017-10-16 12:54:10 +0200

    player movement fix

--- Game/src/entities/Player.java
+++ Game/src/entities/Player.java
@@ -28,27 +28,15 @@ public class Player extends Creature {
 		walking = false;
 		if(Game.getHandler().getKeyManager().getKeyPressed(KeyEvent.VK_DOWN)) {
 			movePlayer(Direction.DOWN);
-			walking = true;
-			facing = Direction.DOWN;
-			this.animation = AssetManager.playerDown;
 		}
 		if(Game.getHandler().getKeyManager().getKeyPressed(KeyEvent.VK_UP)) {
 			movePlayer(Direction.UP);
-			walking = true;
-			facing = Direction.UP;
-			this.animation = AssetManager.playerUp;
 		}
 		if(Game.getHandler().getKeyManager().getKeyPressed(KeyEvent.VK_RIGHT)) {
-			movePlayer(Direction.RIGHT);
-			walking = true;	
-			facing = Direction.RIGHT;
-//			this.animation = AssetManager.playerDown;		
+			movePlayer(Direction.RIGHT);		
 		}
 		if(Game.getHandler().getKeyManager().getKeyPressed(KeyEvent.VK_LEFT)) {
-			movePlayer(Direction.LEFT);
-			walking = true;	
-			facing = Direction.LEFT;
-//			this.animation = AssetManager.playerDown;		
+			movePlayer(Direction.LEFT);	
 		}
 		animation.update();
 	}
@@ -56,19 +44,31 @@ public class Player extends Creature {
 	private void movePlayer(Direction d) {
 		if(d == Direction.UP && !CollisionManager.intersects(this, Direction.UP, speed)) {
 			this.y = Math.max(0,this.y-speed);	
+			walking = true;
+			facing = Direction.UP;
+			this.animation = AssetManager.playerUp;
 		}
 		if(d == Direction.DOWN && !CollisionManager.intersects(this, Direction.DOWN, speed)) {
 			this.y = Math.min(
 						Game.getHandler().getWorldManager().getCurrentWorld().getPixelHeight() - height,
-						this.y+speed);		
+						this.y+speed);	
+			walking = true;
+			facing = Direction.DOWN;
+			this.animation = AssetManager.playerDown;	
 		}
 		if(d == Direction.LEFT && !CollisionManager.intersects(this, Direction.LEFT, speed)) {
 			this.x = Math.max(0,this.x-speed);		
+			walking = true;	
+			facing = Direction.LEFT;
+//			this.animation = AssetManager.playerDown;	
 		}
 		if(d == Direction.RIGHT && !CollisionManager.intersects(this, Direction.RIGHT, speed)) {
 			this.x = Math.min(
 						Game.getHandler().getWorldManager().getCurrentWorld().getPixelWidth() - width,
-						this.x+speed);			
+						this.x+speed);	
+			walking = true;	
+			facing = Direction.RIGHT;
+//			this.animation = AssetManager.playerDown;		
 		}
 	}
 
